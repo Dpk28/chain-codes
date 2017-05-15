d9e41bf5a0f09c7151e61acc7751e6a2c0457e9a
Deepak Bansal <dpkbansal28@gmail.com>
2017-05-15 12:30:57 +0530

    Update voting.go

--- chain-codes/voting.go
+++ chain-codes/voting.go
@@ -1,65 +1,57 @@
 package main
-
-import (
-	"errors"
-	"fmt"
-	"strconv"
+import(
+	"fmt";
+	"strconv";
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+
 )
 
-// SimpleChaincode example simple Chaincode implementation
-type SimpleChaincode struct {
-}
+type VotingChainCode struct{}
 
-var A, B string
-var Aval, Bval, X int
+func (t *VotingChainCode) Init(stub shim.ChaincodeStubInterface, function string, ar[] string) ([]byte, error){
 
-// Init callback representing the invocation of a chaincode
-// This chaincode will manage two accounts A and B and will transfer X units from A to B upon invoke
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	var err error
-
-	if len(args) != 4 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 4")
+	err = stub.PutState("icecream", []byte("0"));
+	if err!=nil {
+		return nil, err;
 	}
-
-	// Initialize the chaincode
-	A = args[0]
-	Aval, err = strconv.Atoi(args[1])
-	if err != nil {
-		return nil, errors.New("Expecting integer value for asset holding")
+	err = stub.PutState("pizza", []byte("0"));
+	if err!=nil {
+		return nil, err;
 	}
-	B = args[2]
-	Bval, err = strconv.Atoi(args[3])
-	if err != nil {
-		return nil, errors.New("Expecting integer value for asset holding")
-	}
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-	return nil, nil
+	return nil,nil;
 }
 
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	// Transaction makes payment of X units from A to B
+func (t *VotingChainCode) Invoke(stub shim.ChaincodeStubInterface, function string, ar[] string) ([]byte, error){
+		var err error
+		var val int
+		var store string
+		option,err := stub.GetState(ar[0]);
+		if err!=nil{
+			return nil, err;
+		}
+		val, err = strconv.Atoi(string(option))
+		store =  strconv.Itoa(val+1);
+		err = stub.PutState(ar[0],[]byte(store));
+		if err!=nil {
+			return nil, err;
+		}
+		return nil, nil;
+}
+
+func (t* VotingChainCode) Query(stub shim.ChaincodeStubInterface, function string, ar[] string) ([]byte, error){
 	var err error
-	X, err = strconv.Atoi(args[0])
-	Aval = Aval - X
-	Bval = Bval + X
-	ts, err2 := stub.GetTxTimestamp()
-	if err2 != nil {
-		fmt.Printf("Error getting transaction timestamp: %s", err2)
-	}
-	fmt.Printf("Transaction Time: %v,Aval = %d, Bval = %d\n", ts, Aval, Bval)
-	return nil, err
+	result,err := stub.GetState(ar[0]);
+	if err!=nil {
+	 	return nil, err;
+	 } 
+	 return []byte(result), nil;
 }
 
-// Query callback representing the query of a chaincode
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	return nil, nil
-}
-
-func main() {
-	err := shim.Start(new(SimpleChaincode))
-	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
+func main(){
+	var err error
+	err = shim.Start(new(VotingChainCode))
+	if err!=nil{
+		fmt.Printf("Error starting voting chaincode: %s", err)
 	}
 }
